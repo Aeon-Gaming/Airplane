16fc3dd439e80b084603b5ff803f0a2e46a24ec5
KrolthAeon <krolth@aeonegaming.com>
2020-07-06 23:08:25 -0500

    this is the second commit

---
+++ Source/Airplane/AirplaneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AirplaneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRPLANE_API AAirplaneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
